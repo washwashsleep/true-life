052ebc9b9befb31c90f09feb9fc0723e40c6b0fa
Simon Sun <blackghost0118@gmail.com>
2015-02-01 03:37:57 +0800

    更改樣式

--- app.js
+++ app.js
@@ -6,20 +6,22 @@ var users = [];
 app.use(express.static(__dirname + '/public'));
 
 var server = require('http').createServer(app);
-var expresspeerserver = ExpressPeerServer(server, {debug: true});
+var expresspeerserver = ExpressPeerServer(server, {
+    debug: true
+});
 
-expresspeerserver.on('connection', function (id) { 
+expresspeerserver.on('connection', function (id) {
     users.push(id);
     console.log('new connection, id is : %s', id);
 });
 
 app.use('/myapp', expresspeerserver);
 
-app.get('/Qoo', function (req, res){
+app.get('/Qoo', function (req, res) {
     res.json(users);
 });
 
-server.listen(9000, function (){
+server.listen(9000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
