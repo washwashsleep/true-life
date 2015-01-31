a2258bb0333e3c6e4d9be30f9673c96a949cdc1e
Dca <dca35311@gmail.com>
2015-01-31 23:00:22 +0800

    好棒棒 輸入ＩＤ可以連線了

--- public/script.js
+++ public/script.js
@@ -1,34 +1,37 @@
-var myId = 'dca001';
-var peer = new Peer(myId, {key: 'j2qmsjunc7zdj9k9'}); 
+var myId = 'peer' + parseInt( Math.random()*1e7 , 10);
+var peer = new Peer(myId, {
+    key: 'j2qmsjunc7zdj9k9'
+    // host: 'localhost', port: 9000, path: '/myapp'
+}); 
 
 peer.on('open', function(id) {
-  	console.log('My peer ID is: ' + id);
+    console.log('My peer ID is: ' + id);
 });
 
 
 peer.on('connection', function(conn) { 
-	console.log('on connection');
+    console.log('on connection');
 
-	window.temp_conn = conn;
-	
-	conn.on('open', function() {
-		// Receive messages
-		conn.on('data', function(data) {
-			console.log('Received', data);
-		});
+    window.temp_conn = conn;
 
-		// Send messages
-		conn.send('Hello!');
-	});
+    conn.on('open', function() {
+        // Receive messages
+        conn.on('data', function(data) {
+            console.log('Received', data);
+        });
+
+        // Send messages
+        conn.send('Hello!');
+    });
 });
 
 
 peer.on('call', function(call) {
-  	call.answer(window.localStream);
+    call.answer(window.localStream);
 
-  	call.on('stream', function(stream) {
-    	$('#their-video').prop('src', URL.createObjectURL(stream));
-  	});
+    call.on('stream', function(stream) {
+        $('#their-video').prop('src', URL.createObjectURL(stream));
+    });
 });
 
 
@@ -48,7 +51,28 @@ function getMyStream () {
     }, function(){ console.log('error'); });
 }
 
+function tryConnect(theirId) {
+
+    console.log('嘗試與 %s 建立連線', theirId);
+
+    var conn = peer.connect(theirId);
+    var call = peer.call(theirId, window.localStream);
+
+    if (!call) {
+        return alert('連線錯誤');
+    }
+
+    call.on('stream', function(stream) {
+        $('#their-video').prop('src', URL.createObjectURL(stream));
+    });
+}
+
 
 $(document).ready(function() {
-	getMyStream();
+    getMyStream();
+    $('#myId').html(myId);
+
+    $('#btn-connect').on('click', function(){
+        tryConnect( $('#theirId').val() );
+    });
 });
