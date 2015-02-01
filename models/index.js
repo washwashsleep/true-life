b706356e31fba27999e4fd9a59feb3f714795d0b
Simon Sun <blackghost0118@gmail.com>
2015-02-01 13:59:29 +0800

    fixed big

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 var mongojs = require('mongojs'),
     db = mongojs('mongodb://128.199.223.114:27017/true_life'),
-    users = db.collection('users');
+    users = db.collection('users'),
+    likes = db.collection('likes');
 
 module.exports = {
-    users: users
+    users: users,
+    likes: likes
 };
